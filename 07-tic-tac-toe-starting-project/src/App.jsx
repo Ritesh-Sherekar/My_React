42b485919fe9608a6728723fa610b6cadee3a4a7
Ritesh <riteshsherekar69@gmail.com>
2025-03-01 06:00:04 +0530

    28Feb_Tic_Tac_Toe

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,21 +1,31 @@
 import { useState } from "react";
+import { WINNING_COMBINATIONS } from "./WINNING_COMBINATIONS.JS";
+
 import Players from "./Components/Players.jsx";
 import GameBoard from "./Components/GameBoard.jsx";
+import Log from "./Components/Log.jsx";
+
+function driveActivePlayer(gameTurns){
+  let currentPlayer = "X";
+
+  if (gameTurns.length > 0 && gameTurns[0].player === "X") {
+    currentPlayer = "O";
+  }
+  return currentPlayer;
+}
 
 function App() {
   const [gameTurns, setGameTurn] = useState([]);
-  const [activePlayer, setActivePlayer] = useState('X');
+  //const [activePlayer, setActivePlayer] = useState('X');
+
+  const activePlayer = driveActivePlayer(gameTurns);
 
   function hendleSelectSquare(rowIndex, colIndex) {
-    setActivePlayer((currentActivePlayer) =>
-      currentActivePlayer === "X" ? "O" : "X"
-    );
+    // setActivePlayer((currentActivePlayer) =>
+    //   currentActivePlayer === "X" ? "O" : "X"
+    // );
     setGameTurn((prevTurns) => {
-      let currentPlayer = 'X';
-
-      if(prevTurns.length > 0 && prevTurns[o].player === 'X'){
-        currentPlayer = 'O';
-      }
+      const currentPlayer = driveActivePlayer(prevTurns);
 
       const updatedTurns = [
         { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
@@ -40,11 +50,9 @@ function App() {
             isActive={activePlayer === "O"}
           />
         </ol>
-        <GameBoard
-          onSelectSquare={hendleSelectSquare}
-          turns={gameTurns}
-        />
+        <GameBoard onSelectSquare={hendleSelectSquare} turns={gameTurns} />
       </div>
+      <Log turn={gameTurns} />
     </main>
   );
 }
